e26e130a57d6d1d3c5cc27d57c142cb42ff1d4fc
enix <43864619+enixbr@users.noreply.github.com>
2020-12-22 14:26:56 -0300

    s
    s

--- commands/antiraid.js
+++ commands/antiraid.js
@@ -6,6 +6,12 @@ module.exports = {
     description: 'Ativa cooldown em todos os canais.',
     execute(message, args) {
     const embed = new Discord.MessageEmbed()
+        if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) {
+          embed.setTitle(':x: Sem permissões o suficiente.')
+          embed.setDescription('')
+          message.reply(embed)
+          return;
+        }
         if (raidMode == false) {
         raidMode = true;
         embed.setTitle(':white_check_mark: Modo Anti-Raid Ativado.')
